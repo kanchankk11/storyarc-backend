3f02ba00eb0cefac42004dfceaa804ed78554f12
Kanchan Nag <nagkanchan5@gmail.com>
2025-11-28 01:44:08 +0530

    Change for Dynamic port on render

--- src/server.js
+++ src/server.js
@@ -16,6 +16,8 @@ app.get("/", (req, res) => {
   res.send("StoryArc Backend Running");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
